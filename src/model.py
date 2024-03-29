975a2c2aa136a3445c0619037d3e53984398b146
UwU-tao <hieungo1410@gmail.com>
2024-03-29 07:45:58 +0700

    add

--- src/model.py
+++ src/model.py
@@ -236,6 +236,6 @@ class Demo(nn.Module):
         bundle_gamma = torch.tanh(self.bundle_freq / psi)
         aff_bundles_feat_ =  aff_bundles_feat * (1 - bundle_gamma.unsqueeze(1))
         hist_bundles_feat_ = hist_bundles_feat * bundle_gamma.unsqueeze(1)
-        scores = aff_bundles_feat @ aff_bundles_feat_.T + hist_bundles_feat @ hist_bundles_feat_.T
+        scores = aff_bundles_feat @ aff_bundles_feat_.t() + hist_bundles_feat @ hist_bundles_feat_.t()
         
         return scores
