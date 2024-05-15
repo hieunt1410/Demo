6e2ef098598b601dfaf9f962d828258228f15911
UwU-tao <hieungo1410@gmail.com>
2024-05-15 17:34:07 +0700

    add

--- src/model.py
+++ src/model.py
@@ -288,7 +288,7 @@ class Demo(nn.Module):
         
         scores = self.evaluate(propagate_result, users)
         # c_list = groupby_apply(bundles, scores, bins=self.num_bundles, reduction='sum').to(device)
-        c_list = [1] * len(q_list)
+        c_list = torch.Tensor([1] * len(q_list)).to(device)
         
         with np.errstate(invalid='ignore'):
             r_list = c_list/(q_list**(2-1.552))
