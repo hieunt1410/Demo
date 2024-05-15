8f81dc541645a4f13d2d08b0e5882263b2dacbc8
UwU-tao <hieungo1410@gmail.com>
2024-05-15 18:00:39 +0700

    add

--- src/model.py
+++ src/model.py
@@ -53,7 +53,7 @@ class Demo(nn.Module):
         # self.get_hist_graph_ori()
         # self.get_agg_graph_ori()
         # self.UI_propagation_graph_ori = self.get_propagation_graph(self.ui_graph)
-        self.UI_aggregation_graph_ori = self.get_user_prop_graph(self.ui_graph)
+        self.UI_propagation_graph_ori = self.get_user_prop_graph(self.ui_graph)
         # self.UI_aggregation_graph_ori = self.get_aggregation_graph(self.ui_graph)
         
         self.UB_propagation_graph_ori = self.get_propagation_graph(self.ub_graph)
