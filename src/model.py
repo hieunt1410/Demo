b8d8264d9b58068a4c57f3d1530473153eb58684
UwU-tao <hieungo1410@gmail.com>
2024-05-16 15:36:11 +0700

    add

--- src/model.py
+++ src/model.py
@@ -99,7 +99,7 @@ class Demo(nn.Module):
         nn.init.xavier_normal_(self.bundles_feat)
         self.items_feat = nn.Parameter(torch.FloatTensor(self.num_items, self.embedding_size))
         nn.init.xavier_normal_(self.items_feat)
-        self.items_pop = nn.Parameter(torch.FloatTensor(self.num_items, self.num_items))
+        self.items_pop = nn.Parameter(torch.FloatTensor(self.num_items, self.embedding_size))
         nn.init.xavier_normal_(self.items_pop)
         self.embedding = nn.Embedding(self.num_users + self.num_items, self.embedding_size)
         nn.init.xavier_normal_(self.embedding.weight)
@@ -200,7 +200,7 @@ class Demo(nn.Module):
     #     return Ufeat, Ifeat, Bfeat
     
     def one_aggregate(self, agg_graph, node_feature, test):
-        aggregated_feature = agg_graph @ self.items_pop @ node_feature 
+        aggregated_feature = agg_graph @ (node_feature + self.items_pop) 
 
         return aggregated_feature
     
