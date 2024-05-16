7f6941e8eaed50d80dd297ba3fbeacf89fc12e7f
UwU-tao <hieungo1410@gmail.com>
2024-05-16 15:52:23 +0700

    add

--- src/model.py
+++ src/model.py
@@ -200,7 +200,7 @@ class Demo(nn.Module):
     #     return Ufeat, Ifeat, Bfeat
     
     def one_aggregate(self, agg_graph, node_feature, test):
-        aggregated_feature = agg_graph @ (node_feature + self.items_pop) 
+        aggregated_feature = agg_graph @ (node_feature) 
 
         return aggregated_feature
     
