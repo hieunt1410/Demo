b0d55968fa1ab3b86e305ac9335729b37a8085cd
UwU-tao <hieungo1410@gmail.com>
2024-06-25 00:43:46 +0700

    add

--- src/model.py
+++ src/model.py
@@ -188,6 +188,7 @@ class Demo(nn.Module):
             #     feats = feats + sign * random_noise
             neighbor_feats = self.cal_edge_weight(graph, feats, test)
             feats = neighbor_feats + self.residual_coff * (feats - ini_feats)
+            feats /= (i + 2)
             feats = F.normalize(feats, p=2, dim=1)
             
             all_feats.append(feats)
