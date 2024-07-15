87315dc28f6ff3f930f51c72e7934e83fc054326
UwU-tao <hieungo1410@gmail.com>
2024-07-15 22:16:38 +0700

    add

--- src/model.py
+++ src/model.py
@@ -172,13 +172,13 @@ class Demo(nn.Module):
             neighbor_feats = self.cal_edge_weight(graph, feats, test)
             feats = neighbor_feats + self.residual_coff * (feats - ini_feats)
             
-            # feats /= (i + 2)
+            feats /= (i + 2)
             feats = F.normalize(feats, p=2, dim=1)
             
             all_feats.append(feats)
             
         all_feats = torch.stack(all_feats, dim=1)
-        all_feats = torch.sum(all_feats, dim=1)
+        all_feats = torch.mean(all_feats, dim=1)
         
         Afeat, Bfeat = torch.split(all_feats, (Afeat.shape[0], Bfeat.shape[0]), 0)
         
